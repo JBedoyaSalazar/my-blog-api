9b552b2df0079acecb4675be324c4ce067dbec9f
Jefred Bedoya <jefred03salazar@gmail.com>
2026-07-14 23:08:07 -0500

    feat: integrate ConfigService into AppService for dynamic app name retrieval in getHello method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 import { UsersService } from './users/users.service';
-import { Env } from './env.model';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly usersService: UsersService,
-    private readonly configService: ConfigService<Env>,
   ) {}
 
   @Get()
   getHello(): string {
-    const appName = this.configService.get('APP_NAME', { infer: true });
-    const message = this.appService.getHello();
-    return `<h1>${message} - ${appName}</h1>`;
+    return `<h1>${this.appService.getHello()}</h1>`;
   }
 
   @Get('tests')
