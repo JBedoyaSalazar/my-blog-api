d43ce4aff2d2b4d5678a66bc1da8feb7e1489f26
Jefred Bedoya <jefred03salazar@gmail.com>
2026-07-14 23:04:28 -0500

    feat: add configuration support with @nestjs/config; implement environment variable handling and update AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
+import { Env } from './env.model';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly usersService: UsersService,
+    private readonly configService: ConfigService<Env>,
+  ) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    const appName = this.configService.get('APP_NAME', { infer: true });
+    const message = this.appService.getHello();
+    return `<h1>${message} - ${appName}</h1>`;
+  }
+
+  @Get('tests')
+  getTests() {
+    return this.usersService.findAllUsers();
   }
 }
