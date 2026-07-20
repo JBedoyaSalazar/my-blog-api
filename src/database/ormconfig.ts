f8783edbf289f10e0c915b11eb3610022cafccc3
Jefred Bedoya <jefred03salazar@gmail.com>
2026-07-20 15:30:24 -0500

    feat: initialize TypeORM DataSource configuration with environment-based settings

--- src/database/ormconfig.ts
+++ src/database/ormconfig.ts
@@ -3,16 +3,30 @@ import { config } from 'dotenv';
 
 config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '5232', 10),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+
+  ...(isProduction
+    ? {
+        url: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+      }),
+
   entities: ['./src/**/*.entity.ts'],
   migrations: ['./src/database/migrations/*.ts'],
   migrationsTableName: 'migrations',
+
   synchronize: false,
   logging: true,
 });
