7320c9c87cdb27aed821a6c6c3f0d113c59ce0a4
Jefred Bedoya <jefred03salazar@gmail.com>
2026-07-20 14:44:22 -0500

    feat: implement main entry point with helmet security middleware and set minimum Node.js version engine

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors({
+    origin: '*'
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
